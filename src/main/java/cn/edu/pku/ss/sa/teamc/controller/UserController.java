6c17c253cc84d8a9b7c5ff00fee6f3f53aadf7c6
Yuanming XU <yuanmingxu@hotmail.com>
2016-04-26 12:05:14 +0800

    covert to web project

---
+++ src/main/java/cn/edu/pku/ss/sa/teamc/controller/UserController.java
@@ -0,0 +1,17 @@
+package cn.edu.pku.ss.sa.teamc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author xuyuanming
+ *
+ * 2016年4月25日下午6:24:17
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	
+}
